36c038d8137de33b9b9c1b195924e9b7da610a0f
armany87 <h.gonzalezrivera2@gmail.com>
2021-07-31 16:07:54 -0500

    Add files via upload

--- nba_2k_league.py
+++ nba_2k_league.py
@@ -111,7 +111,7 @@ with stats_container:
 
 	# 4 --- You import datasets like you always do with pandas
 	# 		if you'd like to import data from a database, you need to set up a database connection
-	data = pd.read_csv('data/nba2kleague_stats.csv')
+	data = get.data('data/nba2kleague_stats.csv')
    
 
 
