56609874482e3240344021e5fd8ac95647c8fc79
armany87 <h.gonzalezrivera2@gmail.com>
2021-07-31 15:52:14 -0500

    Add files via upload

--- nba_2k_league.py
+++ nba_2k_league.py
@@ -111,7 +111,7 @@ with stats_container:
 
 	# 4 --- You import datasets like you always do with pandas
 	# 		if you'd like to import data from a database, you need to set up a database connection
-	data = pd.read_csv('nba2kleague_stats.csv')
+	data = pd.read_csv('streamlit template/nba2kleague_stats.csv')
    
 
 
